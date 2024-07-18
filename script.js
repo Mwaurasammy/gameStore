33d9369d469f40e7f73de3bf5d063b5a3d4bd0f2
SammyMwaura <mwaurasammy500@gmail.com>
2024-07-18 18:53:15 +0300

    faq modal

--- script.js
+++ script.js
@@ -452,6 +452,34 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// Get the modal
+var modal = document.getElementById("faq-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("faq-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("faq-close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 
 
 
